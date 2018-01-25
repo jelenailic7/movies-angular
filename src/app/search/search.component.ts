065a05237c872e170ad0a960ec748e64b5992001
jelenailic7 <ilic.jelena20@gmail>
2018-01-25 21:49:42 +0100

    6 zadatak

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { Movie } from '../shared/models/movie';
 import { MoviesService } from '../shared/service/movies.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html'
 })
 export class SearchComponent  {
+    
+    constructor()
+    {
+        this.router.navigate('/movies');
+    }
 }
